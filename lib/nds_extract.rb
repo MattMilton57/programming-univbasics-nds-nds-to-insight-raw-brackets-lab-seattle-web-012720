7e36b08caa5397462c020eb3ec41a2f52c219ed2
Matt Milton <mattmilton57@gmail.com>
2020-01-18 20:48:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,16 +6,18 @@ require 'pp'
 
 def directors_totals(nds)
 result={}
-
-moviecount=0 
-grossss=0
-dirname = directors_database[0][:name]
-while moviecount < directors_database[0][:movies].length
-grossss+= directors_database[0][:movies][moviecount][:worldwide_gross]
-moviecount+=1
+dircount=0
+while dircount < directors_database.length do
+    moviecount=0 
+    grossss=0
+    dirname = directors_database[dircount][:name]
+        while moviecount < directors_database[dircount][:movies].length do
+        grossss+= directors_database[dircount][:movies][moviecount][:worldwide_gross]
+        moviecount+=1
+        end
+    result[dirname] = grossss
+    dircount+=1
 end
-result[dirname] = grossss
-
 p result
 
 
