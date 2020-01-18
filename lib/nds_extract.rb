f9a9537e317c858059314f6ec5498c4da850bca0
Matt Milton <mattmilton57@gmail.com>
2020-01-18 20:42:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,13 +3,18 @@ require 'directors_database'
 
 require 'pp'
 
+result={}
+
 moviecount=0 
 grossss=0
 while moviecount < directors_database[0][:movies].length
 grossss+= directors_database[0][:movies][moviecount][:worldwide_gross]
 moviecount+=1
 end
-p grossss
+
+
+
+
 def directors_totals(nds)
 
 end
